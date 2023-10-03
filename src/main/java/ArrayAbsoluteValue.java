23e361a5b78db6b4c86e84c715cbc4ce932b40d3
Shakhzod Akhtamov <shakakhtamov@gmail.com>
2023-10-03 21:51:20 +0000

    array absolute value complete

--- src/main/java/ArrayAbsoluteValue.java
+++ src/main/java/ArrayAbsoluteValue.java
@@ -9,6 +9,10 @@ public class ArrayAbsoluteValue {
      * @return the absolute value array of nums.
      */
     public int[] getArrayAbs(int[] nums){
-        return null;
+        int [] absoluteValue= new int[nums.length];
+        for(int i=0; i< nums.length;i++){
+            absoluteValue[i] = Math.abs(nums[i]);
+        } 
+        return absoluteValue;
     }
 }
